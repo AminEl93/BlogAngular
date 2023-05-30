bc3332e4a2f6eb406f9f384785b4da1f5c83b533
AminEl93 <amin_11_iverson@hotmail.com>
2023-05-30 19:34:21 +0200

    Creación de los principales componentes, importación de los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BlogComponent,
+    FormularioComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
