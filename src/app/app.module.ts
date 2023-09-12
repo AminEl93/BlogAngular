8034b517cf6e31247e73bb5c3df6ce9a8143dc0a
AminEl93 <amin_11_iverson@hotmail.com>
2023-09-12 13:28:08 +0200

    Validación del formulario y eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -34,7 +35,8 @@ import { EsParPipe } from './pipes/espar.pipe';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
 
+    public user: any;
+    public campo!: string;
+
+    constructor(){
+        this.user = {
+            nombre: '',
+            apellidos: '',
+            bio: '',
+            genero: ''
+        };
+    }
+
+    ngOnInit() {
+        
+    }
+
+    onSubmit() {
+        alert("Formulario enviado!");
+        console.log(this.user);
+    }
+
+    hasDadoClick() {
+        alert('Has dado click!');
+    }
+
+    hasSalido() {
+        alert('Has salido!');
+    }
 }
